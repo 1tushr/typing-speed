be6114c91b82e121631228709de79e8058102504
Tushar Verma <tuhsar2022@gmail.com>
2023-06-19 21:01:30 +0530

    added data to data base

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,21 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Graph from "./Graph";
+import { auth, db } from "../firebaseConfig";
+import { toast } from "react-toastify";
+
 export default function Stats({
-    wpm,
-    accuracy,
-    correctChars,
-    incorrectChars,
-    missedChars,
-    extraChars,
-    graphData
-  }){
-    let timeSet =new Set();
-    const newGraph=graphData.filter(i=>{
-      if(!timeSet.has(i[0])){
-        timeSet.add(i[0]);
-        return i;
+  wpm,
+  accuracy,
+  correctChars,
+  incorrectChars,
+  missedChars,
+  extraChars,
+  graphData,
+}) {
+  let timeSet = new Set();
+  const newGraph = graphData.filter((i) => {
+    if (!timeSet.has(i[0])) {
+      timeSet.add(i[0]);
+      return i;
+    }
+  });
+
+  const pushDataToDb = () => {
+    if(isNaN(accuracy)){
+      toast.error("Invalid test ", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+      return;
+    }
+    const resultsRef = db.collection("Results");
+    const { uid } = auth.currentUser;
+    resultsRef
+      .add({
+        wpm: wpm,
+        accuracy: accuracy,
+        timeStamp: new Date(),
+        characters: `${correctChars}/${incorrectChars}/${missedChars}/${extraChars}`,
+        userId: uid,
+      })
+      .then(() => {
+        toast.success("Data saved to db", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      })
+      .catch(() => {
+        toast.error("Unable to save data", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
+      });
+  };
+
+  useEffect(() => {
+    const checkUserAndSaveData = () => {
+      if (auth.currentUser) {
+        pushDataToDb();
+      } else {
+        toast.warning("Login to save the result", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
       }
-    })
+    };
+
+    checkUserAndSaveData();
+  }, []);
+
   return (
     <div className="stats-box">
       <div className="left-stats">
@@ -28,9 +103,8 @@ export default function Stats({
           {correctChars}/{incorrectChars}/{missedChars}/{extraChars}
         </div>
       </div>
-      <div className="right-stats">{/*graph*/}
-      
-      <Graph graphData={newGraph}></Graph>
+      <div className="right-stats">
+        <Graph graphData={newGraph}></Graph>
       </div>
     </div>
   );
